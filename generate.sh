ca5a88f38d1dea7bdf9094b3af2197fb4785fb15
wonkwh <wonkwh@nc-and.com>
2025-12-01 18:15:20 +0900

    ✨ change to local VLCKit package

--- generate.sh
+++ generate.sh
@@ -3,22 +3,22 @@
 
 _TAG_VERSION="${1}";
 
-IOS_URL="https://download.videolan.org/pub/cocoapods/unstable/VLCKit-4.0.0a10-d962e05c-17860384.tar.xz"
+# IOS_URL="https://download.videolan.org/pub/cocoapods/unstable/VLCKit-4.0.0a10-d962e05c-17860384.tar.xz"
+IOS_URL="./package/VLCKit.xcframework.tar.gz"
 
 rm -rf .tmp/ || true
 mkdir .tmp/
 
-wget -O .tmp/VLCKit.tar.xz $IOS_URL
-tar -xf .tmp/VLCKit.tar.xz -C .tmp/
+tar -xf ${IOS_URL} -C .tmp/
 
-IOS_LOCATION=".tmp/VLCKit-binary/VLCKit.xcframework"
+IOS_LOCATION=".tmp/VLCKit.xcframework"
 
 ditto -c -k --sequesterRsrc --keepParent "${IOS_LOCATION}" "${IOS_LOCATION}.zip"
 
 #Update package file
 PACKAGE_HASH=$(sha256sum "${IOS_LOCATION}.zip" | awk '{ print $1 }')
-PACKAGE_STRING="Target.binaryTarget(name: \"VLCKit\", url: \"https:\/\/github.com\/g-mrll\/hl-vlckit-ios-spm\/releases\/download\/v${_TAG_VERSION}\/VLCKit.xcframework.zip\", checksum: \"$PACKAGE_HASH\")"
+PACKAGE_STRING="Target.binaryTarget(name: \"VLCKit\", url: \"https:\/\/github.com\/wonkwh\/vlckit-ios-spm\/releases\/download\/v${_TAG_VERSION}\/VLCKit.xcframework.zip\", checksum: \"$PACKAGE_HASH\")"
 echo "Changing package definition for xcframework with hash $PACKAGE_HASH"
 sed -i '' -e "s/let vlcBinary.*/let vlcBinary = $PACKAGE_STRING/" Package.swift
 
-cp -f .tmp/VLCKit-binary/COPYING.txt ./LICENSE
+cp -f .tmp/COPYING.txt ./LICENSE
--- gh_release.sh
+++ gh_release.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gh release create "v${1}" .tmp/VLCKit-binary/VLCKit.xcframework.zip --title "${1}" --notes "${1}";
+gh release create "v${1}" .tmp/VLCKit.xcframework.zip --title "${1}" --notes "${1}";
